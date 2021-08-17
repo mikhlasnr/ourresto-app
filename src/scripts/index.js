4eceff6941f62bb8d424d7d987993ee137c53cfe
DESKTOP-RIGDD5R\minrn <minrnullpo@gmail.com>
2021-08-17 13:13:26 +0700

    create header nav and hero

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,6 @@
-import 'regenerator-runtime'; /* for async await transpile */
-import '../styles/main.css';
+import "regenerator-runtime"; /* for async await transpile */
+import "../styles/main.css";
+import "../styles/responsive.css";
+import "./utils";
 
-console.log('Hello Coders! :)');
+console.log("Hello Coders! :)");
