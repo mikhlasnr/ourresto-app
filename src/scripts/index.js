1e2b49b009ab4400176d9e86b4dba7fdb760083f
DESKTOP-RIGDD5R\minrn <minrnullpo@gmail.com>
2021-08-21 11:49:27 +0700

    devlop done

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,5 +2,62 @@ import "regenerator-runtime"; /* for async await transpile */
 import "../styles/main.css";
 import "../styles/responsive.css";
 import "./utils";
+import data from "../DATA.json";
 
-console.log("Hello Coders! :)");
+document.addEventListener("DOMContentLoaded", function () {
+  console.log(data.restaurants);
+  const posts = document.querySelector("#posts");
+  let card = "<p tabindex='0' class='post-item-empty'>Post Empty</p>";
+  data.restaurants.forEach((item, index) => {
+    if (index === 0) {
+      card = `<article class="post-item" tabindex="0" aria-label="post item">
+              <div class="post-item__thumbnail-container">
+                <img
+                  class="post-item__thumbnail"
+                  src="${item.pictureId}"
+                  alt="restaurant"
+                />
+                <div class="post-item__city">
+                  <p tabindex="0" aria-label="lokasi tempat, kota ${item.city}">
+                    Kota ${item.city}
+                  </p>
+                </div>
+              </div>
+              <div class="post-item__content">
+                <span class="post-item__rating" tabindex="0">rating ${item.rating}</span>
+                <h2 class="post-item__title" tabindex="0">
+                  ${item.name}
+                </h2>
+                <p class="post-item__description" tabindex="0">
+                  ${item.description}
+                </p>
+              </div>
+            </article>`;
+    } else {
+      card += `<article class="post-item" tabindex="0" aria-label="post item">
+              <div class="post-item__thumbnail-container">
+                <img
+                  class="post-item__thumbnail"
+                  src="${item.pictureId}"
+                  alt="restaurant"
+                />
+                <div class="post-item__city">
+                  <p tabindex="0" aria-label="lokasi tempat, kota ${item.city}">
+                    Kota ${item.city}
+                  </p>
+                </div>
+              </div>
+              <div class="post-item__content">
+                <span class="post-item__rating" tabindex="0">rating ${item.rating}</span>
+                <h2 class="post-item__title" tabindex="0">
+                  ${item.name}
+                </h2>
+                <p class="post-item__description" tabindex="0">
+                  ${item.description}
+                </p>
+              </div>
+            </article>`;
+    }
+  });
+  posts.innerHTML = card;
+});
--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -8,6 +8,12 @@ menu.addEventListener("click", function (event) {
   event.stopPropagation();
 });
 
+menu.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    drawer.classList.toggle("open");
+    event.stopPropagation();
+  }
+});
 hero.addEventListener("click", function () {
   drawer.classList.remove("open");
 });
